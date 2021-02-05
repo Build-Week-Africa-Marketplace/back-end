5f33b244bd821c649c4902646f7a38efa0a53921
Heather Beckman <heatherbeckman85@gmail.com>
2021-02-04 19:00:27 -0700

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const userRouter = require("./users/users-router")
 const itemsRouter = require("./items/items-router")
+const cors = require("cors")
 
 const server = express()
+server.use(cors())
 
 server.use(express.json())
 server.get("/", (req, res) => {
